6713d6c138b612e3aa7155c0ac1b2098c9315373
Jahnavi245 <jahnavi25102001@gmail>
2025-03-18 11:27:35 +0530

    feat(add progressbar component):update index export

--- libs/ngx-tailwind-flex-ui/src/index.ts
+++ libs/ngx-tailwind-flex-ui/src/index.ts
@@ -3,4 +3,6 @@ export * from './lib/icon/icon.component';
 export * from './lib/checkbox/checkbox.component';
 export * from './lib/rating/rating.component';
 export * from './lib/progress-bar/progress-bar.component';
+
+export * from './lib/progress-bar/progress-bar.component';
 export * from './lib/chip/chip.component';
